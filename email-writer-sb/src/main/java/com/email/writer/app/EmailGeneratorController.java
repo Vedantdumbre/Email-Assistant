a65787fbefa88377c91c1f0109107e1b2ad4c131
Vedant <vedantdumbre0706@gmail.com>
2025-07-20 20:49:30 +0530

    Added crosorigin to all

--- email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -1,15 +1,14 @@
 package com.email.writer.app;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/email")
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
