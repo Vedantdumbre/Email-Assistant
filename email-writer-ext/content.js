dbee44fdd5e904a6cc82f5f564792b52390f3d86
Vedant <vedantdumbre0706@gmail.com>
2025-08-08 21:52:19 +0530

    Using Motion Observer for injecting my chrome extension functionalities into, Ui of GMail

--- email-writer-ext/content.js
+++ email-writer-ext/content.js
@@ -1,1 +1,23 @@
 console.log("hello! bro");
+
+function injectButton(){
+
+}
+
+const observer = new MutationObserver((mutations) => {
+    for(const mutation of mutations) {
+  const addedNodes = Array.from(mutation.addedNodes);
+  const hasComposeElements = addedNodes.some(node =>
+    node.nodeType === Node.ELEMENT_NODE &&
+    (node.matches('.aDh, .bTC, [role="dialog"]') || node.querySelector('.aDh, .bTC, [role="dialog"]'))
+    );
+    }
+    if (hasComposeElements) {
+        console.log("Compose Window Detected");
+        setTimeout(injectButton, 500);
+    }
+});
+
+observer.observe(document.body, {
+    childList: true, subtree: true
+});
